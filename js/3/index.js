31fc09491821344a3119708e8dccbf4540c926ee
vaida <vaida@exemple.com>
2018-09-13 23:50:00 +0300

    1

---
+++ js/3/index.js
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="js/3/index.js"></script>
+var amzius = prompt("Įveskite savo amžių.") , 0;
+if(amzius > 17){
+  document.write("Sveiki prisijungę.");
+}
+else{
+  document.write("Esate netinkamo amžiaus.");
+}
