39b7ebbf6f9ede90b4e8b10330c6ffc14cf0b584
vaida <vaida@exemple.com>
2018-09-17 11:55:48 +0300

    2

---
+++ 3/index.js
@@ -0,0 +1,10 @@
+
+function amziausCenzas(amzius) {
+
+if(amzius > 17){
+  return "Sveiki atvyke!";
+}
+else{
+  return "Esate netinkamo amžiaus.";
+}
+}
---
+++ 4antras/mano.js
@@ -0,0 +1,15 @@
+function jonoPrisijungimas(v,p) {
+
+  if (v=="Jonas") {
+dokument.write("iveskite pavarde");
+  }
+  else{
+    dokument.write ("Neteisingas prisijungimas");
+  }
+  if(p=="Jonaitis"){
+    return "Sveiki prisijunge";
+  }
+  else{
+    return "bandykite is naujo";
+  }
+}
